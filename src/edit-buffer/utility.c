5bb658d0fa7eec024e1333fddaf65223ef26954c
Sasha Koshka <supergriffin64@gmail.com>
2022-05-26 22:19:01 -0400

    Split EditBuffer into separate files
    edit-buffer.c's thousand line long reign of terror is no more! Now, it has been
    split up into Cursor, EditBuffer, and Utility.

---
+++ src/edit-buffer/utility.c
@@ -0,0 +1,42 @@
+#include "module.h"
+
+/* constrainChange
+ * Constrains a change in initial of amount, to a lower bound of 0 and an upper
+ * bound of bound (bound is not inclusive).
+ */
+size_t constrainChange (size_t initial, int amount, size_t bound) {
+        size_t amountAbs;
+        
+        if (amount < 0) {
+                amountAbs = (size_t)(0 - amount);
+                if (initial >= amountAbs) {
+                        initial -= amountAbs;
+                } else {
+                        initial = 0;
+                }
+        } else {
+                amountAbs = (size_t)amount;
+                initial += amountAbs;
+        }
+        
+        if (initial >= bound) {
+                initial = bound - 1;
+        }
+
+        return initial;
+}
+
+/* addToSizeT
+ * Behaves the same as constrainChange, but does not specify an upper bound.
+ */
+size_t addToSizeT (size_t initial, int amount) {
+        size_t amountAbs;
+        
+        if (amount < 0) {
+                amountAbs = (size_t)(0 - amount);
+                return initial - amountAbs;
+        } else {
+                amountAbs = (size_t)(amount);
+                return initial + amountAbs;
+        }
+}
