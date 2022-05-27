e7948d8871c66776e48a10b4f982fa1dd0a9d8a3
Sasha Koshka <supergriffin64@gmail.com>
2022-05-26 21:18:28 -0400

    Split build targets into separate file
    build-targets.sh now contains the switch statement of build targets.

---
+++ build-targets.sh
@@ -0,0 +1,33 @@
+case $1 in
+all)     buildAll $2      ;;
+release) buildAll release ;;
+"")      buildAll         ;;
+
+redo)
+        clean
+        buildAll $2
+        ;;
+
+clean)
+        clean
+        ;;
+
+run)
+        buildAll && "./$DEBUG_PATH"
+        ;;
+
+lint)
+        clang-tidy \
+        -checks=portability-*,bugprone-*,-bugprone-macro-parentheses \
+        --warnings-as-errors=* "$SRC_PATH"/* -- $FLAGS_CFLAGS
+        ;;
+
+val)
+        buildAll
+        valgrind --tool=memcheck --leak-check=yes $DEBUG_PATH
+        ;;
+
+*)
+        buildModule $1 $2
+        ;;
+esac
